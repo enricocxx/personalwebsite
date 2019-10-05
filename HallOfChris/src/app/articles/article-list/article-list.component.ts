81a995159909be0d249cd0fa4cfd3579deb9384b
nlaundry <nlaundry@localhost.localdomain>
2019-10-05 18:58:42 -0400

    Structured articile list template
    Added material grid with 3 columns.
    Loops through an array of articles.
    Adjusted height of this component to fill space.

--- HallOfChris/src/app/articles/article-list/article-list.component.ts
+++ HallOfChris/src/app/articles/article-list/article-list.component.ts
@@ -8,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ArticleListComponent implements OnInit {
 
   constructor() { }
+  
+  articles: any[] = ['1','2','3','4','5','6'];
 
   ngOnInit() {
+    console.log(this.articles);
   }
 
 }
